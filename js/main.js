e31890528631ecb08cc9eddc3f3e5b06a3de5217
Polina Kuchina <Lermenua@yandex.ru>
2019-01-07 23:29:48 +0300

    min.js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ try {
 } catch (err) {
   isStorageSupport = false;
 }
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
@@ -23,11 +24,13 @@ link.addEventListener("click", function (evt) {
     login.focus();
   }
 });
+
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
 });
+
 form.addEventListener("submit", function (evt) {
   if (!login.value || !feedback.value || !email.value) {
     evt.preventDefault();
@@ -40,6 +43,7 @@ form.addEventListener("submit", function (evt) {
     }
   }
 });
+
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();

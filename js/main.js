8b27324c2c6b0c236fd53a7b6302d46fc268af9c
Polina Kuchina <Lermenua@yandex.ru>
2019-01-07 23:23:14 +0300

    Добавляет анимацию + скрипт работы модального окна и карту

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var link = document.querySelector(".button-info");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".button-close-feedback");
+var login = popup.querySelector("[name=name]");
+var form = popup.querySelector("form");
+var feedback = popup.querySelector("[name=feedback]");
+var email = popup.querySelector("[name=email]");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  if (storage) {
+    login.value = storage;
+    feedback.focus();
+  } else {
+    login.focus();
+  }
+});
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !feedback.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
